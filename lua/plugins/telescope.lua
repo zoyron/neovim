8cce1f0e20fc922e829c6d90d72d6b3bdc3afcbc
zoyron <sgrarora3@gmail.com>
2024-01-02 17:56:09 +0530

    moved telescope to plugins directory from plugins.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,5 @@
-
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-    }
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+-- or                              , branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
 }
