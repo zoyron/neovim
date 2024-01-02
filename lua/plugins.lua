d5674fb16f23f5ff58fabaeeedb3d255c2520dee
zoyron <sgrarora3@gmail.com>
2024-01-02 17:45:28 +0530

    moving plugins to separate files initialized

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,11 +1,5 @@
 -- Plugings list
 return {
-{
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-},
 {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
