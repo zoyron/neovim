3d330846363657316a97e030df2ad41ea43bba52
zoyron <sgrarora3@gmail.com>
2024-01-02 17:51:36 +0530

    moved telescope from plugins.lua to plugins directory

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,4 +4,4 @@ return {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
 -- or                              , branch = '0.1.x',
       dependencies = { 'nvim-lua/plenary.nvim' }
-},
+}
