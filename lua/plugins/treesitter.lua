a5046c779f8a558d4910469ddf0bded19fdaa49a
zoyron <sgrarora3@gmail.com>
2024-01-02 17:57:39 +0530

    corrected the mistake in treesitter plugin file

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,2 @@
 
 return {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-{
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-}
