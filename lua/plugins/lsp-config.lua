7d947da49e214fdb6708b39ffdaaad5fcd2a81cb
zoyron <sgrarora3@gmail.com>
2024-01-05 19:33:55 +0530

    added the keymap to hover function of the lualsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,6 +18,7 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
     end
   }
 }
