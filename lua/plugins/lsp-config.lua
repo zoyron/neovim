30deb8e173d579492103a38a1621a41aa7151f54
zoyron <sgrarora3@gmail.com>
2024-01-05 19:52:54 +0530

    added the keymap for goto definition

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,6 +19,7 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
     end
   }
 }
