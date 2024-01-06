eababd63b290dcd3bcd28a7e54ae8b3671248e1c
zoyron <sgrarora3@gmail.com>
2024-01-06 14:00:00 +0100

    added the codeactions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,6 +20,7 @@ return {
       lspconfig.lua_ls.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n',';ca', vim.lsp.buf.code_action, {})
     end
   }
 }
