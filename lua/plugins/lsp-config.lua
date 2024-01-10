8c43f66a1ec2a9b9314b82a4127dd6e428f503f9
zoyron <sgrarora3@gmail.com>
2024-01-11 04:33:36 +0530

    installed the tsserver

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
+        ensure_installed = { "lua_ls", "tsserver" }
       })
     end
   },
@@ -18,6 +18,7 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n',';ca', vim.lsp.buf.code_action, {})
