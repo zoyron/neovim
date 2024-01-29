edde1eb0f52a2d4ca1ea80f0c261fd504be82109
zoyron <sgrarora3@gmail.com>
2024-01-30 00:09:01 +0530

    installed the python lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -27,6 +27,9 @@ return {
       lspconfig.clangd.setup({
         capabilities = capabilities
       })
+      lspconfig.pyright.setup({
+        capabilities = capabilities
+      })
       vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n',';ca', vim.lsp.buf.code_action, {})
