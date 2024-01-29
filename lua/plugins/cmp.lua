edf8e6b6fd783dd4fadb5b8b1317be471f6716ac
zoyron <sgrarora3@gmail.com>
2024-01-29 20:16:28 +0530

    removed the luasnip module as it got redundant due to refactoring

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,7 +6,6 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
-		"saadparwaiz1/cmp_luasnip",
 		"hrsh7th/cmp-nvim-lua",
 		"windwp/nvim-autopairs",
 		"onsails/lspkind-nvim",
