a13c27ee10a9488ef2b974ab2fbae8f3c0185c25
zoyron <sgrarora3@gmail.com>
2024-01-29 12:16:28 +0530

    added the clang lsp to the lsp config file

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,6 +19,7 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({})
+      lspconfig.clangd.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n',';ca', vim.lsp.buf.code_action, {})
