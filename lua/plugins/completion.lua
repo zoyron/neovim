5160285f7e2be59bfcf4fba8d19fd94acb0906eb
zoyron <sgrarora3@gmail.com>
2024-01-29 22:23:21 +0530

    done with the basic tab functioning in the refactored code

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -32,24 +32,24 @@ return {
       local cmp = require("cmp")
       require("luasnip.loaders.from_vscode").lazy_load()
       --local lsp_kind = require("lspkind")
-      --local cmp_next = function(fallback)
-      --  if cmp.visible() then
-      --    cmp.select_next_item()
-      --  elseif require("luasnip").expand_or_jumpable() then
-      --    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-      --  else
-      --    fallback()
-      --  end
-      --end
-      --local cmp_prev = function(fallback)
-      --  if cmp.visible() then
-      --    cmp.select_prev_item()
-      --  elseif require("luasnip").jumpable(-1) then
-      --    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-      --  else
-      --    fallback()
-      --  end
-      --end
+      local cmp_next = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif require("luasnip").expand_or_jumpable() then
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+        else
+          fallback()
+        end
+      end
+      local cmp_prev = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif require("luasnip").jumpable(-1) then
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+        else
+          fallback()
+        end
+      end
 
       --lsp_kind.init()
       ---@diagnostic disable-next-line
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,10 +16,17 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.clangd.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
       vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n',';ca', vim.lsp.buf.code_action, {})
