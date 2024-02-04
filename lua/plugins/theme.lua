ead644364edbd03778e614028d8999fd2bf8fb12
zoyron <sgrarora3@gmail.com>
2024-02-04 16:37:05 +0530

    changed the vim command for the nord theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -24,7 +24,8 @@ return {
   "shaunsingh/nord.nvim",
   lazy = false,
   priority = 1000,
+  opts = {},
   config = function ()
-   vim.cmd([[colorscheme nord]])
+    vim.cmd.colorscheme "nord"
   end
 }
