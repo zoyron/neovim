6481d0776cf33b7dc64e21ce947f60828dccc9c8
zoyron <sgrarora3@gmail.com>
2024-02-06 21:21:40 +0530

    added emmet and pyright in ensure installed

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "clangd" }
+        ensure_installed = { "lua_ls", "tsserver", "clangd", "pyright", "emmet_ls" }
       })
     end
   },
