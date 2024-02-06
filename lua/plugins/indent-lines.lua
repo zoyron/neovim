f820dca8438181f748de96d13e6be70b73098992
zoyron <sgrarora3@gmail.com>
2024-02-06 17:25:36 +0530

    made the indentation better, and removed the ugly and resource hungy scope variable from the indent plugin

--- lua/plugins/indent-lines.lua
+++ lua/plugins/indent-lines.lua
@@ -1,8 +1,27 @@
+---@diagnostic disable: undefined-global
 return {
   "lukas-reineke/indent-blankline.nvim",
+  opts = {
+    indent = {
+      char = "│",
+      tab_char = "│",
+    },
+    scope = { enabled = false },
+    exclude = {
+      filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+    },
+  },
   main = "ibl",
-  opts = {},
-  config = function ()
-    require("ibl").setup()
-  end
 }
