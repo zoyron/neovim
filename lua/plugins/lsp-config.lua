5eaf2a2e8b83266351f091183974a5bf2699d403
zoyron <sgrarora3@gmail.com>
2024-02-06 19:22:59 +0530

    added the emmet support for html

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -30,6 +30,9 @@ return {
       lspconfig.pyright.setup({
         capabilities = capabilities
       })
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities
+      })
       vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n',';ca', vim.lsp.buf.code_action, {})
