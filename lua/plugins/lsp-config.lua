08808ddba39d1f2007e848ce0890244736fb9b14
zoyron <sgrarora3@gmail.com>
2024-03-12 23:21:41 +0530

    added the dignostic shortcut

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -36,6 +36,7 @@ return {
       vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n',';ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', ';gl', vim.diagnostic.open_float, {})
     end
   }
 }
