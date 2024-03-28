5ba7da59d99c4552c0cb349df9f4314b8de23435
zoyron <sgrarora3@gmail.com>
2024-03-28 15:47:42 +0530

    modified the cpp template and the initial cursor position in the cpp file

--- templates/skeleton.cpp
+++ templates/skeleton.cpp
@@ -1,6 +1,40 @@
 #include <bits/stdc++.h>
+
+#define lli long long int
+#define F first
+#define S second
+#define pb push_back
+#define endl '\n'
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
 using namespace std;
 
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
 
+inline int power(int a, int b)
+{
+	int x = 1;
+	while (b)
+	{
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
 
-int main(){}
+void solve(){}
+
+int32_t main(){
+  int t = 1;
+  //cin >> t;
+  while(t--) solve();
+  return 0;
+}
