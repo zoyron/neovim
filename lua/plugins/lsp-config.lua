346365229017645777b88b63a55243def423e2c5
zoyron <sgrarora3@gmail.com>
2025-06-21 17:57:36 +0530

    regular update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -6,39 +6,49 @@ return {
     end
   },
   {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "clangd", "pyright", "emmet_ls" }
-      })
-    end
-  },
-  {
     "neovim/nvim-lspconfig",
+    dependencies = { "williamboman/mason.nvim" },
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
+
+      -- Simple manual setup for each server
       lspconfig.lua_ls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" }
+            }
+          }
+        }
       })
+
       lspconfig.ts_ls.setup({
         capabilities = capabilities
       })
+
       lspconfig.clangd.setup({
         capabilities = capabilities
       })
+
       lspconfig.pyright.setup({
         capabilities = capabilities
       })
+
       lspconfig.emmet_ls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+        filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact" }
       })
+
       lspconfig.glsl_analyzer.setup({
         capabilities = capabilities
       })
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
+
+      -- Keymaps
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n',';ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', ';ca', vim.lsp.buf.code_action, {})
       vim.keymap.set('n', ';gl', vim.diagnostic.open_float, {})
     end
   }
